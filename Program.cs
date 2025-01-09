29929e7ca6893c4c313cc9f1d27412ff99d91b7f
Akbar <babaevakbars@gmail.com>
2025-01-09 23:01:37 +0500

    Задача 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Program
+{
+    class Lesson6
+    {
+        public static int SumArray(int[] array)
+        {
+            int sum = 0;
+            int index = 0;
+            foreach(int i in array)
+            {
+                sum += i;
+                Console.WriteLine($"array[{index}] - {i}");
+                index++;
+            }
+            return sum;
+        } 
+        static void Main(string[] args)
+        {
+            //Задача 1
+            Random rand = new Random();
+            int[] array = new int[4];
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0, 100);
+            }
+            int resultOfMethod = SumArray(array);
+            Console.WriteLine($"Сумма всех чисел в массиве: {resultOfMethod}");
+        }
+    }
+}
--- obj/Debug/net8.0/Lesson6.AssemblyInfo.cs
+++ obj/Debug/net8.0/Lesson6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d30d28c8433ac93d68425a19a73fb5b082bf3ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
